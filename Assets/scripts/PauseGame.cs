2bb91491e68e24a6a4069cdcbb7a3fdb77bd3413
max <imax.peas@gmail.com>
2025-07-23 23:39:46 +0300

    Homework3 undone UnityBalance3D

--- Assets/scripts/PauseGame.cs
+++ Assets/scripts/PauseGame.cs
@@ -12,19 +12,21 @@ public class PauseGame : MonoBehaviour
         }
     }
 
-    void TogglePause()
+    public void TogglePause()
     {
         if (!isPaused)
         {
             Time.timeScale = 0f;
             AudioListener.pause = true;
             Debug.Log("paused");
+            isPaused = true;
         }
         else
         {
             Time.timeScale = 1f;
             AudioListener.pause = false;
             Debug.Log("unpaused");
+            isPaused=false;
         }
     }
 }
--- Assets/scripts/SceneController.cs
+++ Assets/scripts/SceneController.cs
@@ -3,6 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    public static void ChangeScene()
+    {
+        int CurrentSceneID = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(CurrentSceneID + 1 );
+    }
     public static void RestartLevel()
     {
         int currentSceneID = SceneManager.GetActiveScene().buildIndex;
